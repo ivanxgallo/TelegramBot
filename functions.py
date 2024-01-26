21890f3fcde06fe01a635ab714d5571623181f21
ivanxgallo <ivan.gallo@ug.uchile.cl>
2024-01-26 04:01:23 -0300

    Algunos ajustes

--- functions.py
+++ functions.py
@@ -1,5 +1,3 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from team_optimazer import *
 import itertools
 
 # --------------- DEFINING SOME VARIABLES -------------------#
